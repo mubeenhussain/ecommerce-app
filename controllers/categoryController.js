85044797c15448c1260f1cce10c88ad0b4967f29
mubeenhussain <mubeen.hussain@xcentricservices.com>
2024-11-12 23:07:11 +0500

    category api created

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -26,3 +26,82 @@ export const createCategoryController =async (req,res) => {
         console.log(error);
     }
 }
+
+export const updateCategoryController = async (req,res) => {
+    try {
+         const {name} = req.body;
+         const {id} = req.params;
+         const category = await categoryModal.findByIdAndUpdate(id,{
+            name:name,
+            slug:slugify(name)
+         },{new:true});
+         res.status(200).send({
+            success:true,
+            message:"Category updated successfully!",
+            category
+         })
+    } catch (error) {
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while updating category"
+        })
+    }
+}
+
+//get all category
+export const categoryController = async (req,res) => {
+    try {
+       const category = await categoryModal.find({});
+       res.status(200).send({
+        success:true,
+        message:"All Categories",
+        category
+       })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while getting all categories"
+        })
+    }
+}
+
+//get single category
+export const singleCategoryController = async (req,res) => {
+    try {
+        const {slug} = req.params;
+       const category = await categoryModal.findOne({slug});
+       res.status(200).send({
+        success:true,
+        category
+       })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while getting single Category"
+        })
+    }
+}
+
+//deleteCategoryController
+export const deleteCategoryController = async (req,res) => {
+    try {
+        const {id} = req.params;
+        await categoryModal.findByIdAndDelete(id);
+        res.status(200).send({
+         success:true,
+         
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,
+            error,
+            message:"Error while deleting single Category"
+        })
+    }
+}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import { isAdmin, requireSignIn } from './../middlewares/authMiddleware.js';
-import { createCategoryController } from '../controllers/categoryController.js';
+import { categoryController, createCategoryController, deleteCategoryController, singleCategoryController, updateCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router();
 
 //routes
+//create-category
 router.post(
     '/create-category',
     requireSignIn,
     isAdmin,
     createCategoryController
 )
+//update category
+router.put('/update-category/:id', requireSignIn, isAdmin, updateCategoryController)
 
+//get all category
+router.get('/get-category', categoryController);
+
+//get single category
+router.get('/single-category/:slug', singleCategoryController);
+
+//delete single category
+router.delete(' /:id', requireSignIn, isAdmin, deleteCategoryController)
 
 export default router;
